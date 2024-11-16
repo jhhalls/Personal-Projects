5a047bf7b677dc850673c9dec739b9d9acc0a545
Rohit Raj Jalheria <rjalheira@gmail.com>
2024-11-16 14:09:49 +0530

    Update data_visualizer.py
    changed from function to manual

--- Interactive_Data_Visualization/data_visualizer.py
+++ Interactive_Data_Visualization/data_visualizer.py
@@ -159,10 +159,10 @@ if selected_option_2 == "Describe":
         st.write(describe)
     # st.write(describe)
 elif selected_option_2 == "Info":
-    # buffer = io.StringIO()
-    # df.info(buf=buffer)
-    # buffer_get_value = buffer.getvalue()
-    get_df_info(df)
+    buffer = io.StringIO()
+    df.info(buf=buffer)
+    buffer_get_value = buffer.getvalue()
+    st.text(buffer_get_value)
 elif selected_option_2 == "Null Values":
     null_data = df.isna().sum()
     total_null = df.isna().sum().sum()
